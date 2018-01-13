e025eafee6df8e0770d0c8a9a3fd3935332a20ce
Mayur Muralidhar <11.mayur@gmail.com>
2018-01-13 17:21:01 -0600

    Changed client HTML to include React elements

--- client/main.js
+++ client/main.js
@@ -3,7 +3,14 @@ import ReactDOM from 'react-dom'
 import {Meteor} from 'meteor/meteor'
 
 Meteor.startup(function() {
+  let title = 'Hello Internet!'
   let name = 'Mayur';
-  let jsx = <p>Hello {name}!</p>;
+  let jsx = (
+    <div>
+      <h1>{title}</h1>
+      <p>Hello {name}!</p>
+      <p>This is my second paragraph.</p>
+    </div>
+  );
   ReactDOM.render(jsx,document.getElementById('app'));
 });
