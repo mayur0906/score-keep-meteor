3211f34e5f408b1593102aef3ac5594627b08932
Mayur Muralidhar <11.mayur@gmail.com>
2018-01-14 17:22:11 -0600

    Classes in ES6

--- server/main.js
+++ server/main.js
@@ -4,7 +4,19 @@ import {Tracker} from 'meteor/tracker'
 
 
 Meteor.startup(() => {
+  class Person{
+    constructor(name='Anonymous',age=0){
+      this.name = name;
+      this.age = age;
+    }
+    getGreeting(){
+      return `Hi! I am ${this.anme}`;
+    }
+    getDescription(){
+      return `${this.name} is ${this.age} year(s) old.`;
+    }
+  }
 
-
-
+  let me = new Person('Mayur',24);
+  console.log(me.getDescription());
 });
