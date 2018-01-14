9f0d1a96e0bf0e979f3465c6b7ad36f6d6b22ab2
Mayur Muralidhar <11.mayur@gmail.com>
2018-01-13 20:31:08 -0600

    MongoDB Client Server Sync

--- client/main.js
+++ client/main.js
@@ -5,30 +5,11 @@ import {Tracker} from 'meteor/tracker'
 
 import {Players} from './../imports/api/players.js';
 
-Tracker.autorun(function() {
-  console.log('Players list',Players.find().fetch());
-})
 
-
-
-const players = [{
-  _id:1,
-  name:'Alice',
-  score:99
-},{
-  _id:2,
-  name:'Bob',
-  score:-1
-},{
-  _id:3,
-  name:'Catherine',
-  score:-12
-}];
-
-const renderPlayers = function() {
+const renderPlayers = function(playersList) {
   let numbers = [{val:9},{val:2},{val:3},{val:101}];
 
-  return players.map(function(player){
+  return playersList.map(function(player){
     return <p key={player._id}>{player.name} has {player.score} points.</p>;
   });
 
@@ -36,15 +17,25 @@ const renderPlayers = function() {
 }
 
 Meteor.startup(function(playersList) {
-  let title = 'Hello Internet!'
-  let name = 'Mayur';
-  let jsx = (
-    <div>
-      <h1>{title}</h1>
-      <p>Hello {name}!</p>
-      <p>This is my second paragraph.</p>
-      {renderPlayers()}
-    </div>
-  );
-  ReactDOM.render(jsx,document.getElementById('app'));
+  Tracker.autorun(function() {
+    let players=Players.find().fetch();
+    let title = 'Hello Internet!'
+    let name = 'Mayur';
+    let jsx = (
+      <div>
+        <h1>{title}</h1>
+        <p>Hello {name}!</p>
+        <p>This is my second paragraph.</p>
+        {renderPlayers(players)}
+      </div>
+    );
+    ReactDOM.render(jsx,document.getElementById('app'));
+  });
+
+  Players.insert({
+    name: 'Dominic',
+    score: 6
+  });
+  // console.log(Players.find().fetch());
+
 });
--- server/main.js
+++ server/main.js
@@ -1,10 +1,10 @@
 import {Meteor} from 'meteor/meteor'
 import {Players} from './../imports/api/players.js'
+import {Tracker} from 'meteor/tracker'
+
 
 Meteor.startup(function(){
-  Players.insert({
-    name: 'Bob',
-    score: 6
-  });
   console.log(Players.find().fetch());
+  
+
 });
