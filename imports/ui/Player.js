4aa7198bf8ec2f7117b8766a0311d38f1baeed2f
Mayur Muralidhar <11.mayur@gmail.com>
2018-01-15 16:08:41 -0600

    Learned BEM pattern for CSS

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -10,16 +10,16 @@ export default class Player extends React.Component {
         <p>
           {this.props.player.name} has {this.props.player.score} points.
         </p>
-        <button onClick = {() =>Players.update(
+        <button className="button button--round" onClick = {() =>Players.update(
           this.props.player._id,
           {$inc : {score:-1}}
         )}>
           -1
         </button>
-        <button onClick = {() =>Players.update(this.props.player._id,{$inc : {score:1}})}>
+        <button className="button button--round" onClick = {() =>Players.update(this.props.player._id,{$inc : {score:1}})}>
           +1
         </button>
-        <button onClick = {() =>Players.remove(this.props.player._id)}>
+        <button className="button button--round" onClick = {() =>Players.remove(this.props.player._id)}>
           X
         </button>
       </div>
