f3cb2803af8b8242e6364550894a25f62d67eb2f
Mayur Muralidhar <11.mayur@gmail.com>
2018-02-01 00:53:14 -0600

    Added colors to players in 1st, 2nd and 3rd positions.

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -5,14 +5,14 @@ import {Players} from './../api/players.js';
 
 export default class Player extends React.Component {
   render() {
-    let itemClassName = `item`;
+    let itemClassName = `item item--position-${this.props.player.rank}`;
     return (
-      <div key={this.props.player._id} className="item">
+      <div key={this.props.player._id} className={itemClassName}>
         <div className="player">
           <div>
             <h3 className="player__name">{this.props.player.name} </h3>
             <p className="player__stats">
-              {this.props.player.rank} {this.props.player.position} {this.props.player.score} points.
+              {this.props.player.position} place - {this.props.player.score} point(s).
             </p>
           </div>
           <div className="player__actions">
